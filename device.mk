888899f732a468990679ea77190b32fafc4b1f47
Woomymy <woomy@woomy.be>
2023-03-02 20:57:04 +0100

    X6833B: Build bootctrl into vendor
    Signed-off-by: Woomymy <woomy@woomy.be>
    Change-Id: Icdc56ceab2fe3b54c8538574d406dd4ce1f0a5de
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -38,6 +38,8 @@ PRODUCT_PACKAGES += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-service \
+    android.hardware.boot@1.2-mtkimpl \
     android.hardware.boot@1.2-mtkimpl.recovery
 
 # Dynamic partitions
