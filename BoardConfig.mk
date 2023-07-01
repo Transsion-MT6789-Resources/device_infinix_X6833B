61dec5aeda910c5c1f0ad8eb23025a4dc0ac9a3d
bengris32 <bengris32@protonmail.ch>
2023-07-01 17:14:30 +0700

    X6833B: Define Bootloader and Platform flags
    Change-Id: I8a4aad74ec90124cc35847c64d4db23f97af7ad7
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,5 +21,13 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := x6833b
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6789
+BOARD_HAS_MTK_HARDWARE := true
+
 # Inherit the proprietary files
 include vendor/infinix/X6833B/BoardConfigVendor.mk
