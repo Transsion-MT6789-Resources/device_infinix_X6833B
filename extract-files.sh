4502d0692b3f48ed85f6f3ff4dda09d51dc15d98
Jiyong Park <jiyong@google.com>
2023-07-24 11:08:23 +0100

    X6833B: gnss: Remove ndk_platform backend. Use the ndk backend.
    The ndk_platform backend will soon be deprecated because the ndk backend
    can serve the same purpose. This is to eliminate the confusion about
    having two variants (ndk and ndk_platform) for the same ndk backend.
    
    Bug: 161456198
    Test: m
    Merged-In: Id16039722fd70d4e2e934a0988f13a768ce833ec
    Change-Id: Id16039722fd70d4e2e934a0988f13a768ce833ec
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- extract-files.sh
+++ extract-files.sh
@@ -55,6 +55,10 @@ fi
 
 function blob_fixup {
     case "$1" in
+        vendor/bin/hw/android.hardware.gnss-service.mediatek |\
+        vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so)
+            "$PATCHELF" --replace-needed "android.hardware.gnss-V1-ndk_platform.so" "android.hardware.gnss-V1-ndk.so" "$2"
+            ;;
         vendor/lib*/hw/mt6789/vendor.mediatek.hardware.pq@2.15-impl.so)
             "$PATCHELF" --replace-needed "libutils.so" "libutils-v32.so" "$2"
             ;;
