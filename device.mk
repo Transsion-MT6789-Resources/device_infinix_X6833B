3a7d1a3adee3ae5ea2951b1ce206867dc39a46ac
bengris32 <bengris32@protonmail.ch>
2023-07-25 12:59:56 +0100

    X6833B: Import Widevine DRM blobs
    Change-Id: Ia8706a1435c5386331c3fce0f600f03f7aefe96e
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -97,6 +97,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey
 
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.4.vendor
+
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
