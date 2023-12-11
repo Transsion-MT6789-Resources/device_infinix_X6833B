ba2741e9eb27b17f61f1229c9ef479bbd8224eb9
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2023-12-11 09:03:18 +0700

    X6833B: Define tap to wake node
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -117,6 +117,7 @@ TARGET_BOARD_PLATFORM := mt6789
 BOARD_HAS_MTK_HARDWARE := true
 
 # Power
+TARGET_TAP_TO_WAKE_NODE := /proc/gesture_function
 TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 
 # Properties
