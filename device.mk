b608c8138ccf69711fa4f32e2c5ae48c3a787bd3
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2023-12-13 00:59:21 +0700

    X6833B: Include `hardware/transsion` soong namespace
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -234,7 +234,8 @@ PRODUCT_SHIPPING_API_LEVEL := 31
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/mediatek
+    hardware/mediatek \
+    hardware/transsion
 
 # Updatable APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
